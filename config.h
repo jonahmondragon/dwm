9f92e8d78827ea78996bb0076dd789f9a390344a
Jonah Mondragon <mail@jonahmondragon.xyz>
2023-04-05 08:33:49 -0600

    Fixed xrdb color settings and removed conflict marker

--- config.h
+++ config.h
@@ -287,11 +287,10 @@ static Key keys[] = {
 	{ MODKEY,			XK_End,	spawn,		SHCMD("brightness down")},
 	{ 0,			XK_Home,	spawn,		SHCMD("rotate-screen")},
 
-<<<<<<< Updated upstream
     { 0,            XK_KP_Insert,           spawn,  SHCMD("elogind-conf toggle suspend")}, // Keypad 0
     { 0,            XK_KP_Delete,           spawn,  SHCMD("elogind-conf toggle hibernation")},
     { 0,            XK_KP_End,              spawn,  SHCMD("cinema toggle")}, // Keypad 1
-    { ShiftMask,            XK_KP_End,      spawn,  SHCMD("cinema only")}, // Keypad 1
+    { ShiftMask,    XK_KP_End,              spawn,  SHCMD("cinema only")}, // Keypad 1
     { 0,            XK_KP_Down,	    togglescratch,	{.ui = 2}}, // Keypad 2
     { ShiftMask,    XK_KP_Down,	            spawn,	SHCMD("rotate-screen down")}, // Keypad 2
     { 0,			XK_KP_Next,		        xrdb,   {.v = NULL }}, // Keypad 3
@@ -302,24 +301,10 @@ static Key keys[] = {
     { ShiftMask,    XK_KP_Right,            spawn,  SHCMD("rotate-screen right")}, // Keypad 6
     { 0,            XK_KP_Home,             spawn,  SHCMD("")}, // Keypad 7
     { 0,            XK_KP_Up,               spawn,  SHCMD("")}, // Keypad 8
-    { ShiftMask,    XK_KP_Up,               spawn, SHCMD("rotate-screen up")}, // Keypad 8
+    { ShiftMask,    XK_KP_Up,               spawn,  SHCMD("rotate-screen up")}, // Keypad 8
     { 0,            XK_KP_End,              spawn,  SHCMD("")}, // Keypad 9
     { 0,            XK_KP_Add,              spawn,  SHCMD("")},
-    { 0,            XK_KP_Subtract,              spawn,  SHCMD("killall screenkey || screenkey &")},
-=======
-    { 0,    XK_KP_Insert,           spawn,  SHCMD("suspend_mode_toggle")}, // Keypad 0
-    { 0,    XK_KP_End,              spawn,  SHCMD("cinema toggle")}, // Keypad 1
-    { 0,    XK_KP_Down,	        togglescratch,	{.ui = 2} }, // Keypad 2
-    { 0,    XK_KP_Next,             spawn,  SHCMD("")}, // Keypad 3
-    { 0,    XK_KP_Left,             spawn,  SHCMD("")}, // Keypad 4
-    { 0,    XK_KP_Begin,            spawn,  SHCMD("")}, // Keypad 5
-    { 0,    XK_KP_Right,            spawn,  SHCMD("")}, // Keypad 6
-    { 0,    XK_KP_Home,             spawn,  SHCMD("")}, // Keypad 7
-    { 0,    XK_KP_Up,               spawn,  SHCMD("")}, // Keypad 8
-    { 0,    XK_KP_End,              spawn,  SHCMD("")}, // Keypad 9
-    { 0,    XK_KP_Add,              spawn,  SHCMD("")},
-    { 0,    XK_KP_Subtract,              spawn,  SHCMD("killall screenkey || screenkey &")},
->>>>>>> Stashed changes
+    { 0,            XK_KP_Subtract,         spawn,  SHCMD("killall screenkey || screenkey &")},
 
 	{ 0, XF86XK_AudioMute,		spawn,		SHCMD("pamixer -t; kill -44 $(pidof dwmblocks)") },
 	{ 0, XF86XK_AudioRaiseVolume,	spawn,		SHCMD("pamixer -i 2; kill -44 $(pidof dwmblocks)") },
